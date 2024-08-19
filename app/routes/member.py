9720ee3adc472b5e2ed7ba5a22f001168d74bdfa
[evollivia] <[ytafxt@naver.com]>
2024-08-19 11:05:54 +0900

    member 라우터 및 관련파일 추가

---
+++ app/routes/member.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+member_router = APIRouter()
+templates = Jinja2Templates(directory='views/templates')
+
+@member_router.get('/')
+async def index(req: Request):
+    return templates.TemplateResponse('member/index.html', {'request': req})
+
+@member_router.get("/join", response_class=HTMLResponse)
+async def join(req: Request):
+    return templates.TemplateResponse('member/join.html', {'request': req})
+
+@member_router.get("/login", response_class=HTMLResponse)
+async def login(req: Request):
+    return templates.TemplateResponse('member/login.html', {'request': req})
+
+@member_router.get("/myinfo", response_class=HTMLResponse)
+async def myinfo(req: Request):
+    return templates.TemplateResponse('member/myinfo.html', {'request': req})
+
+
+
+
+
+
