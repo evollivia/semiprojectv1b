7a52af46de2c792585d7dee310d114ca7b2432ab
[evollivia] <[ytafxt@naver.com]>
2024-08-26 13:38:36 +0900

    갤러리 서비스에 업로드 기능 및 폼 데이터 처리 구현

--- app/routes/gallery.py
+++ app/routes/gallery.py
@@ -9,6 +9,8 @@ from starlette.templating import Jinja2Templates
 
 from app.dbfactory import get_db
 from app.schema.gallery import NewGallery
+from app.service.gallery import GalleryService
+from app.service.gallery import get_gallery_data, process_upload
 
 gallery_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
@@ -29,26 +31,14 @@ async def write(req: Request):
 
 
 @gallery_router.post("/write", response_class=HTMLResponse)
-async def write(req: Request, title: str = Form(...), userid: str = Form(...),
-                contents: str = Form(...), files: List[UploadFile] = File(...)):
-    print(title, userid, contents)
-    print(files)
+async def write(req: Request, gallery: NewGallery = Depends(get_gallery_data), files: List[UploadFile] = File(...)):
+    try:
+        print(gallery)
+        attachs = await process_upload(files)
+        print(attachs)
+    except Exception as ex:
+        print(f'writeok 오류발생 {str(ex)}')
 
-    UPLOAD_PATH = 'C:/java/nginx-1.26.2/html/cdn/img'
-    attaches = []   # 업로드된 파일 정보를 저장하기 위해 리스트 생성
-    today = datetime.today().strftime('%Y%m%d%H%M%S')   # UUID 생성
-    for file in files:
-        if file.filename != '' and file.size > 0:
-            nfname = f'{today}{file.filename}'
-            # os.path.join(A, B) => A/B (경로생성)
-            fname = os.path.join(UPLOAD_PATH, nfname)    # 업로드할 파일경로 생성
-            content = await file.read() # 업로드할 파일의 내용을 비동기로 읽음
-            with open(fname, 'wb') as f:
-                f.write(content)
-            attach = [nfname, file.size]    # 업로드된 파일 정보를 리스트에 저장
-            attaches.append(attach)
-
-    print(attaches)
     return templates.TemplateResponse('gallery/write.html', {'request': req})
 
 
