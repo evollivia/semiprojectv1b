789de233926f278797123466f5993b0f7b3959d7
[evollivia] <[ytafxt@naver.com]>
2024-08-27 14:20:40 +0900

    sqlalchemy orm을 이용해서 1대1, 1대n 관계 설정

--- app/model/gallery.py
+++ app/model/gallery.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.model.base import Base
 
@@ -15,7 +15,9 @@ class Gallery(Base):
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str]
+    attachs = relationship('GalAttach', back_populates='gallery')    # 하나의 gallery는 하나 이상의 attach가 존재 (1:n)
 
+# back_populates : 양방향 관계 설정, 관계의 상호 참조
 class GalAttach(Base):
     __tablename__ = 'galattach'
 
@@ -24,3 +26,4 @@ class GalAttach(Base):
     fname: Mapped[str] = mapped_column(nullable=False)
     fsize: Mapped[int] = mapped_column(default=0)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    gallery = relationship('Gallery', back_populates='attachs')    # 하나의 attach는 하나의 gallery에 속함 (1:1)
