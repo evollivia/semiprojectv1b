cd914f3bce9bf1197486af766a9793f35e7a563c
[evollivia] <[ytafxt@naver.com]>
2024-08-27 12:20:30 +0900

    gallery view 관련 코드 v2 추가 select문 하나로 구현

--- app/routes/gallery.py
+++ app/routes/gallery.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 from fastapi import APIRouter, Request, Depends, UploadFile, File
@@ -14,7 +15,7 @@ gallery_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 @gallery_router.get("/list/{cpg}", response_class=HTMLResponse)
-async def list(req: Request, cpg: int, db: Session = Depends(get_db)):
+async def glist(req: Request, cpg: int, db: Session = Depends(get_db)):
     try:
 
         stpgb = int((cpg - 1) / 10) * 10 +1
@@ -53,10 +54,13 @@ async def writeok(req: Request, gallery: NewGallery = Depends(get_gallery_data),
 @gallery_router.get("/view/{gno}", response_class=HTMLResponse)
 async def view(req: Request, gno: int, db: Session =Depends(get_db)):
     try:
-        rs1, rs2 = GalleryService.selectone_gallery(gno, db)
-
+        rows = GalleryService.selectone_gallery(gno, db)
+        gallery_dict = defaultdict(list)
+        for row in rows:
+            gallery, gal_attach = row
+            gallery_dict[gallery].append(gal_attach)
         return templates.TemplateResponse('gallery/view.html',
-                                          {'request': req, 'gallery': rs1, 'galattach': rs2})
+                                          {'request': req, 'galleries': gallery_dict.items()})
 
     except Exception as ex:
         print(f'▷▷▷ view 오류발생 {str(ex)}')
