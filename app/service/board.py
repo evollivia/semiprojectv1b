f6febd83df2541311945facbe469d0ae3db6d201
[evollivia] <[ytafxt@naver.com]>
2024-08-29 10:53:44 +0900

    게시글 본문보기 라우트/서비스 코드 추가

--- app/service/board.py
+++ app/service/board.py
@@ -41,3 +41,13 @@ class BoardService:
 
         except SQLAlchemyError as ex:
             print(f'▶▶▶ find_select_board 오류 발생 : {str(ex)}')
+
+    @staticmethod
+    def selectone_board(bno, db):
+        try:
+            stmt = select(Board).where(Board.bno == bno)
+            result = db.execute(stmt)
+            return result
+
+        except SQLAlchemyError as ex:
+            print(f'▶▶▶ selectone_board에서 오류 발생 : {str(ex)}')
