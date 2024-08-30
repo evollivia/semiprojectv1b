8be09e9a4249ca544b1420571005625adb2716f9
[evollivia] <[ytafxt@naver.com]>
2024-08-30 11:18:42 +0900

    게시판 삭제 기능 구현

--- app/service/board.py
+++ app/service/board.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, or_, update, insert, func
+from sqlalchemy import select, or_, update, insert, func, delete
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import contains_eager
 
@@ -95,4 +95,16 @@ class BoardService:
 
         except SQLAlchemyError as ex:
             print(f'▶▶▶ insert_rreply에서 오류 발생 : {str(ex)}')
+            db.rollback()
+
+    @staticmethod
+    def delete_board(db, bno):
+        try:
+            stmt = delete(Board).where(Board.bno == bno)
+            result = db.execute(stmt)
+            db.commit()
+            return result
+
+        except SQLAlchemyError as ex:
+            print(f'▶▶▶ delete_board에서 오류 발생 : {str(ex)}')
             db.rollback()
