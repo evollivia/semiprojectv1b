cea8ab2f8245f325b413c4939917e1b7fc03fd23
c_dock <153040099+evollivia@users.noreply.github.com>
2024-09-25 14:45:50 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -4,10 +4,10 @@ from pydantic.v1 import BaseSettings
 class Settings(BaseSettings):
     userid: str = 'ubuntu'
     passwd: str = 'ubuntu'
-    # dbname: str = 'clouds2024'
+    #dbname: str = 'clouds2024'
     dbname: str = 'ubuntu'
     dburl: str = 'localhost'
     dbconn: str = f'sqlite:///app/{dbname}.db'
-    # dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
+    #dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
 
 config = Settings()
